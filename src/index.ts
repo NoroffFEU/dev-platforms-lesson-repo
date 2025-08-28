7db7c26019ed7ff308deef89c2b3a9794dc896c3
Connor O'Brien <connorbobrien@gmail.com>
2025-08-28 10:31:26 -0300

    Add array route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = 3000;
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const products: Product[] = [
+  { id: 1, name: "Laptop", price: 1200 },
+  { id: 2, name: "Headphones", price: 150 },
+  { id: 3, name: "Keyboard", price: 80 },
+];
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello" });
+});
+
+app.get("/products", (req, res) => {
+  res.json(products);
+  res.json;
 });
 
 app.listen(PORT, () => {
