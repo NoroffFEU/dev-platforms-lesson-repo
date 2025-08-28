d1929165309ca5a342448e66f2c7491cf595c395
Connor O'Brien <connorbobrien@gmail.com>
2025-08-28 20:57:19 -0300

    Add route params examples

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,20 @@ app.get("/", (req, res) => {
 
 app.get("/products", (req, res) => {
   res.json(products);
-  res.json;
+});
+
+app.get("/products/:id", (req, res) => {
+  console.log("req.params", req.params.id);
+
+  const { id } = req.params;
+
+  res.json(`ID is ${id}`);
+});
+
+app.get("/products/:productId/reviews/:reviewId", (req, res) => {
+  const { productId, reviewId } = req.params;
+
+  res.json(`productId is ${productId}, reviewId is ${reviewId}`);
 });
 
 app.listen(PORT, () => {
