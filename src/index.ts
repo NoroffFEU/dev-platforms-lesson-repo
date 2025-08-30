5779eb7d9b5f216252422a6b3372e16fa5d80dc6
Connor O'Brien <connorbobrien@gmail.com>
2025-08-30 16:47:03 -0300

    Custom middleware example

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
-app.use(cors({ origin: "http://127.0.0.1:5500" }));
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    console.log(`Duration: ${duration}`);
+  });
+
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello" });
