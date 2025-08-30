939ad856884681f9aa21e576193e907e0a04b571
Connor O'Brien <connorbobrien@gmail.com>
2025-08-30 10:37:13 -0300

    Middleware intro

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,24 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-interface Product {
-  id: number;
-  name: string;
-  price: number;
-}
+app.use((req, res, next: NextFunction) => {
+  console.log("Middleware here");
+  next();
+});
 
-const products: Product[] = [
-  { id: 1, name: "Laptop", price: 1200 },
-  { id: 2, name: "Headphones", price: 150 },
-  { id: 3, name: "Keyboard", price: 80 },
-];
+app.use((req, res, next) => {
+  console.log("Middleware two");
+  next();
+});
 
-app.get("/products/:id", (req, res) => {
-  const id = Number(req.params.id);
+app.get("/", (req, res) => {
+  console.log("I am the route handler");
 
-  const product = products.find((product) => product.id === id);
-  console.log(product);
-
-  if (!product) {
-    return res.status(404).json({ error: "Product not found" });
-  }
-
-  res.json(product);
+  res.json({ message: "Hello" });
 });
 
 app.listen(PORT, () => {
