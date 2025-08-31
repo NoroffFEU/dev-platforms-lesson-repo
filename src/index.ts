82e5b3813cdfc491824c5424cc9089f67040cb78
Connor O'Brien <connorbobrien@gmail.com>
2025-08-31 10:52:10 -0300

    Accessing headers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -10,41 +10,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-function reqDuration(req: Request, res: Response, next: NextFunction) {
-  const start = Date.now();
+app.get("/", (req, res) => {
+  // console.log(req.headers);
+  console.log(req.headers.authorization);
+  console.log(req.headers["content-type"]);
 
-  res.on("finish", () => {
-    const duration = Date.now() - start;
-    console.log(`Duration: ${duration}`);
-  });
-
-  next();
-}
-
-function mwOne(req: Request, res: Response, next: NextFunction) {
-  console.log("One");
-
-  next();
-}
-
-function mwTwo(req: Request, res: Response, next: NextFunction) {
-  console.log("Two");
-  next();
-}
-
-// app.use("/api", reqDuration);
-
-app.get("/", mwOne, mwTwo, (req, res) => {
   res.json({ message: "Hello" });
 });
-
-// app.get("/api/users", (req, res) => {
-//   res.json({ message: "Users" });
-// });
-
-// app.get("/api/products", (req, res) => {
-//   res.json({ message: "Procucts" });
-// });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
